b12fa8cb47ae1c7e3512656df79a8e89a1814b1c
Shazy021 <shoplik.konstantin@yahoo.com>
2025-05-30 18:46:29 +0300

    Обновление диспетчера: подключены роутеры регистрации и меню, удалён базовый роутер

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -1,11 +1,12 @@
 from aiogram import Dispatcher
-from handlers import base_router
+from handlers import registration_router, menu_router
 
 def get_dispatcher():
     """Создание и настройка диспетчера"""
     dp = Dispatcher()
     
     # Позже здесь будут подключены роутеры
-    dp.include_router(base_router)
+    dp.include_router(registration_router)
+    dp.include_router(menu_router)
     
     return dp
