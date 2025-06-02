d2343bd0c001fd14039742ec2d0f2857fe98bdd2
Shazy021 <shoplik.konstantin@yahoo.com>
2025-06-02 18:53:03 +0300

    Расширение инициализации хендлеров: добавлены роутеры для заявок, их модерации и управления олимпиадами

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,5 +1,15 @@
 from .menu import router as menu_router
 from .registration import router as registration_router
 from .delete_account import router as account_del_router
+from .application_moderation import router as application_moderation_router
+from .application import router as application_router
+from .olympiad_management import router as olympiad_management_router
 
-__all__ = ['registration_router', 'menu_router', 'account_del_router']
+__all__ = [
+    'registration_router',
+    'menu_router',
+    'account_del_router',
+    'application_moderation_router',
+    'application_router',
+    'olympiad_management_router'
+]
