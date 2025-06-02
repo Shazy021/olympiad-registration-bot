4b23769ca229600328c5ef18473c7200b5122d1f
Shazy021 <shoplik.konstantin@yahoo.com>
2025-06-02 18:54:37 +0300

    Расширение инициализации роутеров: добавлены роутеры для удаления аккаунта, модерации заявок, заявок и управления олимпиадами

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -1,5 +1,12 @@
 from aiogram import Dispatcher
-from handlers import registration_router, menu_router
+from handlers import (
+    registration_router,
+    menu_router,
+    account_del_router,
+    application_moderation_router,
+    application_router,
+    olympiad_management_router
+)
 
 def get_dispatcher():
     """Создание и настройка диспетчера"""
@@ -8,5 +15,9 @@ def get_dispatcher():
     # Позже здесь будут подключены роутеры
     dp.include_router(registration_router)
     dp.include_router(menu_router)
+    dp.include_router(account_del_router)
+    dp.include_router(application_moderation_router)
+    dp.include_router(application_router)
+    dp.include_router(olympiad_management_router)
     
     return dp
